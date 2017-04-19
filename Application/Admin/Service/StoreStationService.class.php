2238ed0d9b0e52ee30d5ff6529632911c50229d9
artoo <ppjz.hr@gmail.com>
2017-04-19 23:06:48 +0800

    权限

---
+++ Application/Admin/Service/StoreStationService.class.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+namespace Admin\Service;
+
+
+class StoreStationService{
+	
+    /**
+     * 根据角色显示查询条件
+     */
+	public function listConditionByRole($role_key){
+	    
+	}
+}
